d57b3a8a8d29503e347cbf5530a60a8840453ce2
Matheussmaced <mateusbyakuya3@gmail.com>
2024-06-25 11:31:31 -0300

    feat: adding customers class

--- src/main/java/restapi/crm/rest_api/controller/ClientController.java
+++ src/main/java/restapi/crm/rest_api/controller/ClientController.java
@@ -1,15 +1,18 @@
 package restapi.crm.rest_api.controller;
 
+import java.util.List;
+
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
+
+import restapi.crm.rest_api.model.Customers;
 
 @RestController
 @RequestMapping("/clientes")
 public class ClientController {
 
   @GetMapping
-  public String hello() {
-    return "Olá, mundo";
+  public List<Customers> listar() {
+    
   }
-}
---
+++ src/main/java/restapi/crm/rest_api/model/Customers.java
@@ -0,0 +1,14 @@
+package restapi.crm.rest_api.model;
+
+public class Customers {
+  private Long id;
+  private String name;
+
+  public String getName() {
+    return name;
+  }
+
+  public void setName(String name) {
+    this.name = name;
+  }
+}
