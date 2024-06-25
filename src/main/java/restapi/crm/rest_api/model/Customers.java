68cf917b666e8a92dea8d244b61a14b38a98a341
Matheussmaced <mateusbyakuya3@gmail.com>
2024-06-25 11:45:14 -0300

    feat: finalizing the clients class and adding a table in db h2

--- src/main/java/restapi/crm/rest_api/model/Customers.java
+++ src/main/java/restapi/crm/rest_api/model/Customers.java
@@ -1,7 +1,13 @@
 package restapi.crm.rest_api.model;
 
+import jakarta.persistence.*;
+
+@Entity // table db
 public class Customers {
+  @Id
+  @GeneratedValue(strategy = GenerationType.IDENTITY)
   private Long id;
+  @Column(nullable = false) // not null!
   private String name;
 
   public String getName() {
@@ -11,4 +17,30 @@ public class Customers {
   public void setName(String name) {
     this.name = name;
   }
+
+  @Override
+  public int hashCode() {
+    final int prime = 31;
+    int result = 1;
+    result = prime * result + ((id == null) ? 0 : id.hashCode());
+    return result;
+  }
+
+  @Override
+  public boolean equals(Object obj) {
+    if (this == obj)
+      return true;
+    if (obj == null)
+      return false;
+    if (getClass() != obj.getClass())
+      return false;
+    Customers other = (Customers) obj;
+    if (id == null) {
+      if (other.id != null)
+        return false;
+    } else if (!id.equals(other.id))
+      return false;
+    return true;
+  }
+
 }
